eddba47496887efa6db7b63ae942e1fbc4ced178
BPM37 <142173862+BPM37@users.noreply.github.com>
2023-12-12 18:23:12 +0100

    Add files via upload

---
+++ py/mon_chart_reading.py
@@ -0,0 +1,80 @@
+reading = {'support':
+           """
+            you have what is known as an anafa yoga this is giving you a very good personality a personality
+            which is pleasing to others these planets can make you a good speaker respected in all
+            society and actually you tend to get appreciation for your work these planets can also support your health
+            """,
+           
+           'support_ven':
+                   """
+                    life gives you support from females you're a pleasing person in society women support you.
+                    """,
+           'support_mar':"""life will give you help from your brothers they can have your back""",
+           'support_jup':"""you get support from teachers gurus""",
+
+            'support_mec':"""from younger people perhaps sister or even when you are older from a daughter when you have""",
+           
+           'no_support':
+           """
+            Life is giving you a very not good personality a personality
+            which is not pleasing to others, life make you either not a good speaker nor respected in all
+            society and actually you tend to not get appreciation for your work. your health needs to be guarded.
+            """,
+           
+           
+           'resources':
+               """You can be self-made for you work really hard to get your own
+                resources but resources are also coming to you it's a two-way street""",
+           
+           'no_resources':
+               """You can not be self-made for you do not work really hard to get your own
+                resources and resources are not  also coming to you it's a two-way street""",
+           
+           'resources_mar':""" You work really hard to make your own wealth in life but you may find that your brothers
+            help you as well""",
+           'resources_ven':"""Women give you resources women support you but its still about you working hard""",
+           'resources_sat':"""You have the persistence and you have patience""",
+           'resources_jup':""" You receive help from your teachers gurus and wisdom comes to you in times of need.
+            You have enormous ability to understand shastra and spiritual understanding""",
+           
+           'high_tide':"""Life is pushing all the things you need in life towards you to attain your desires""",
+           'no_high_tide':"""Life is not pushing all the things you need in life towards you to attain your desires""",
+           #**********************************PLANET WITH MOON***********************************************************
+           'sat_with_moon':"""makes you a bit withdrawn but serious and dependable.""",
+            'jup_with_moon':"""makes you perhaps over-optimistic over-expansive sometimes but sociable.""",
+            'ven_with_moon':""" brings many resources to you and many good friends.""",
+
+            'mas_with_moon':
+            """You definitely have increased ability to earn and to gain in life it can be a wealth
+                giving yoga""",
+
+            'mec_with_moon':"""gives you extreme magnetism and communication ability.""",
+           #***********************************************************************************************
+           'fifth_and_ninth_title':
+           """Increased difficulty on your personality in society and a difficult person to get on with""",
+           
+           'mas_fifth_or_ninth':
+           """You are very aggressive and you could make quick decisions but they can be wrong decisions
+                you need to try to have more patience, there's a lot of anger which can disturb relationships.""",
+           'sat_fifth_or_ninth':
+           """
+            Depression is meant to come into your life and it brings your mood right down, and
+            from a materialistic point of view, you can even be made to be anti-social at times
+            and a real melancholic overseeing the dark side of life.
+            But actually from a spiritual point of view, it's desirable because it gives you a
+            realistic and honest appraisal of this maya of reality you are not taken in by
+            what you see in front of you, you know the spiritual truth behind it
+            """,
+           'benefics_fifth_or_ninth':
+           """it's favorable mostly it's going to bring you out into society in a favorable way it's going to give you
+            a pleasing personality and it's going to draw people easily to you.""",
+           
+           'kamajuruma_yoga':"""your mind is free from too much attachment and therefore you have peace of mind
+            but at the same time you have a certain lack of support and resources to your life.
+            YOU NEED TO APPLY A REMEDY HERE""",
+           'fire':"""confidence to act out in society""",
+           'earth':"""are very sensitive to other people""",
+           'air':"""are excellent moving in society, excellent communicators with all sorts of people.""",
+           'water':"""very ambitious to make the most of opportunities in society""",
+
+           }
